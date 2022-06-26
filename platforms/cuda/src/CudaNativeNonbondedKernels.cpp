ad92d0b07bd5b04aba8e52ea8590aa1b0f975652
Charlles Abreu <abreu@eq.ufrj.br>
2022-06-26 17:45:53 -0400

    Compatibilization with OpenMM 7.7.0

--- platforms/cuda/src/CudaNativeNonbondedKernels.cpp
+++ platforms/cuda/src/CudaNativeNonbondedKernels.cpp
@@ -507,8 +507,8 @@ void CudaCalcNativeNonbondedForceKernel::initialize(const System& system, const 
                             cufftSetStream(dispersionFftBackward, pmeStream);
                         }
                     }
-                    CHECK_RESULT(cuEventCreate(&pmeSyncEvent, cu.getEventFlags()), "Error creating event for NativeNonbondedForce");
-                    CHECK_RESULT(cuEventCreate(&paramsSyncEvent, cu.getEventFlags()), "Error creating event for NativeNonbondedForce");
+                    CHECK_RESULT(cuEventCreate(&pmeSyncEvent, CU_EVENT_DISABLE_TIMING), "Error creating event for NonbondedForce");
+                    CHECK_RESULT(cuEventCreate(&paramsSyncEvent, CU_EVENT_DISABLE_TIMING), "Error creating event for NonbondedForce");
                     int recipForceGroup = force.getReciprocalSpaceForceGroup();
                     if (recipForceGroup < 0)
                         recipForceGroup = force.getForceGroup();
