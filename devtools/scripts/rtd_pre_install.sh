8b309aae13f5c55f426b497392bbe0d08e61ca4d
Charlles Abreu <abreu@eq.ufrj.br>
2022-11-19 12:49:38 -0500

    Setup ReadTheDocs building procedure

---
+++ devtools/scripts/rtd_pre_install.sh
@@ -0,0 +1,14 @@
+PYTHON_PATH=$(which python)
+CONDA_PREFIX=${PYTHON_PATH%"/bin/python"}
+echo "CONDA_PREFIX=${CONDA_PREFIX}"
+mkdir build
+cd build
+cmake .. \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
+-DOPENMM_DIR=${CONDA_PREFIX} \
+-DPMESLICING_BUILD_OPENCL_LIB=OFF \
+-DPMESLICING_BUILD_CUDA_LIB=OFF
+make
+make install
+make PythonInstall
